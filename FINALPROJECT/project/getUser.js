5d97d4cf65df644909ddab27c0a5fab224c7b22b
Rimler1 <cht03hunt@gmail.com>
2026-07-24 13:34:32 -0500

    Add files via upload

--- FINALPROJECT/project/getUser.js
+++ FINALPROJECT/project/getUser.js
@@ -1,8 +1,9 @@
 //Tharrin Cooper 7/25/2026
 
-//After user clicks a button ask the user to enter a username from a list if invalid ask again
-//Then display the users username and the any info they entered about themselfs
-//Add a customize button to change a selected users message
+//After user clicks the display button ask the user to enter a username from a list if invalid ask again
+//Then display the users username and the any message associated with that user
+//When user clicks the customize button ask the user to enter a username than allow them to change the message assicuated with the user
+//Ask for a password befor allowing you to change that users message then display that user with their new message
 
 "use strict";
 
@@ -13,23 +14,23 @@ const userNames = document.querySelector(`.user-btn`);
 const users = {
     Ozi: {
         name:`Ozi`,
-
-        message:`Enjoys long walks`
+        password:`Password1`,
+        msg:`Enjoys long walks`
     },
     Inza: {
         name:`Inza`,
-
-        message:`Enjoys reading`
+        password:`Password2`,
+        msg:`Enjoys reading`
     },
     xXTopDogXx: {
         name:`xXTopDogXx`,
-
-        message:`Enjoys video games`
+        password:`Password3`,
+        msg:`Enjoys video games`
     },
     JohnDoe: {
         name:`JohnDoe`,
-
-        message:`Enjoys movies`
+        password:`Password4`,
+        msg:`Enjoys movies`
     }
 }
 
@@ -39,7 +40,7 @@ function displayUser(userNames){
     const user = users[userNames];
     //Change Style
     username.textContent = user.name;
-    info.textContent = user.message;
+    message.textContent = user.msg;
 }
 
 //Handle Display Button
@@ -47,7 +48,7 @@ function handleDisplayClick(){
     //Create Local User
     let user;
     //Get User Input
-    user = prompt(`Enter username from list`,`enter username`)
+    user = prompt(`Enter username from list`,`enter username`);
     //Find Valid User
     for(let key in users){
         if(user === users[key].name){
@@ -65,35 +66,48 @@ function handleDisplayClick(){
     alert(`Invalid User: try again`);
     handleDisplayClick();
 }
-//Handle Random Button
+//Handle Customize Button
 function handleCustomizeClick(){
-    //Create Local User
+    //Create Local Variables
     let user;
-    let newMessage;
+    let lock;
+    let newMsg;
     //Get User Input
-    user = prompt(`Enter username from list`,`enter username`)
+    user = prompt(`Enter username from list`,`enter username`);
     //Find Valid User
     for(let key in users){
         if(user === users[key].name){
             //Get User Input
-            newMessage = prompt(`Enter new message for ${user}`,`enter message`)
-            //User Canceled
-            if(newMessage === null){
-                alert(`Canceling...`);
-                return;
-            }
-            //Message Blank?
-            while(newMessage.trim() === ``){
-                alert(`Message Blank: try again`);
-                newMessage = prompt(`Enter new message for ${user}`,`enter message`)
+            lock = prompt(`Enter password for ${user}`,`enter password`);
+            //Correct Password?
+            while(lock !== users[key].password){
+                alert(`Incorrect Password: try again`);
+                lock = prompt(`Enter password for ${user}`,`enter password`);
                 //User Canceled
-                if(newMessage === null){
+                if(lock === null){
                     alert(`Canceling...`);
                     return;
                 }
             }
-            //Change selected user message
-            users[key].message = newMessage;
+            //Get User Input
+            newMsg = prompt(`Enter new msg for ${user}`,`enter msg`);
+            //User Canceled
+            if(newMsg === null){
+                alert(`Canceling...`);
+                return;
+            }
+            //Message Blank?
+            while(newMsg.trim() === ``){
+                alert(`msg Blank: try again`);
+                newMsg = prompt(`Enter new msg for ${user}`,`enter msg`);
+                //User Canceled
+                if(newMsg === null){
+                    alert(`Canceling...`);
+                    return;
+                }
+            }
+            //Change selected User Message
+            users[key].msg = newMsg;
             //Display user
             displayUser(user);
             return;
@@ -109,5 +123,5 @@ function handleCustomizeClick(){
     handleCustomizeClick();
 }
 //Listen for clicks
-displayBtn.addEventListener("click", handleDisplayClick)
-customizeBtn.addEventListener("click", handleCustomizeClick)
+displayBtn.addEventListener("click", handleDisplayClick);
+customizeBtn.addEventListener("click", handleCustomizeClick);
